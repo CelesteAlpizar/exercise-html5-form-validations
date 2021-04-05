4a62bebb1397a114fa4d549d4d61a5c756717ded
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-04-05 18:11:20 +0000

    Form Validation Complete

--- index.js
+++ index.js
@@ -19,42 +19,81 @@ function Validation() {
     let errorMessages = []
     
     if (CardNumber.value.length != 16) {
-        errorMessages.push("Card Number must have 16 characters")
+        errorMessages.push("Card Number must have 16 characters");
+        InputColorChange(CardNumber);
+    } else {
+        InputRegularStyle(CardNumber);
     }
 
     if(CVCNumber.value.length != 4){
-        errorMessages.push("CVC Number must have 4 characters")
+        errorMessages.push("CVC Number must have 4 characters");
+        InputColorChange(CVCNumber);
+    } else {
+        InputRegularStyle(CVCNumber);
     }
 
     if (Amount.value == "" || Amount.value == "0" ) {
-        errorMessages.push("Amount is required")
+        errorMessages.push("Amount is required");
+        InputColorChange(Amount);
+    } else {
+        InputRegularStyle(Amount);
     }
 
     if (FirstName.value === "") {
-        errorMessages.push("First Name is required")
+        errorMessages.push("First Name is required");
+        InputColorChange(FirstName);
+    } else {
+        InputRegularStyle(FirstName);
     }
 
     if (LastName.value === "") {
-        errorMessages.push("Last Name is required")
+        errorMessages.push("Last Name is required");
+        InputColorChange(LastName);
+    } else {
+        InputRegularStyle(LastName);
     }
 
     if (City.value === "") {
-        errorMessages.push("City is required")
+        errorMessages.push("City is required");
+        InputColorChange(City);
+    } else {
+        InputRegularStyle(City);
     }
 
     if (State.value === "Pick a state" ) {
-        errorMessages.push("State is required")
+        errorMessages.push("State is required");
+        InputColorChange(State);
+    } else {
+        InputRegularStyle(State);
     }
 
     if (PostalCode.value === "") {
-        errorMessages.push("Postal Code is required")
+        errorMessages.push("Postal Code is required");
+        InputColorChange(PostalCode);
+    } else {
+        InputRegularStyle(PostalCode);
     }
 
     if (Message.value == "") {
-        errorMessages.push("Message is required")
+        errorMessages.push("Message is required");
+        InputColorChange(Message);
+    } else {
+        InputRegularStyle(Message);
     }
 
     if (errorMessages.length > 0) {
         errorElement.innerText = errorMessages.join(", ");
-    } 
+    }
+
+    if (errorMessages.length == 0){
+        errorElement.innerText = errorMessages
+    }
+}
+
+function InputColorChange(input) {
+    input.style.background="#f8d7da";
+}
+
+function InputRegularStyle(input) {
+    input.style.background=null
 }
